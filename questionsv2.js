ba4ed6a132acad73ae8d71f6c42e7d332a18d025
JGreenOS <jsciteach@gmail.com>
2021-01-14 13:52:55 -0500

    more debugging

---
+++ questionsv2.js
@@ -0,0 +1,31 @@
+var questions = [
+        {
+            stem: "What type of variable cannot be reassigned?",
+            choice: ["var", "let", "const", "none"],
+            correct: "const"
+        },
+    
+        {
+            stem: "Timers in Javascript are typically measured in which unit of time?",
+            choices: ["minutes", "seconds", "flops", "milliseconds"],
+            correct: "milliseconds"
+        },
+    
+        {
+            stem: "What common structure does the DOM resemble?",
+            choices: ["house", "shed", "shrub", "tree"],
+            correct: "tree"
+        },
+    
+        {
+            stem: "What company developed Javascript?",
+            choices: ["Apple", "Microsoft", "IBM", "Netscape"],
+            correct: "Netscape"
+        },
+    
+        {
+            stem: "Which of these are not a Javascript Event Handler?",
+            choices: ["onclick", "onmouseover", "onsubmit", "continue"],
+            correct: "continue"
+        }
+    ]
