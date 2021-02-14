d653badd7c74c9b511ddc02e9aafb5c0a3cd9e26
JGreenOS <jsciteach@gmail.com>
2021-02-14 11:23:01 -0500

    redoing with pseudocode

--- mainscript.js
+++ mainscript.js
@@ -1,229 +1,46 @@
-
-//holding my questions in an arrayt with the correct answer
-var myQuestions = [
-    {
-        question: "What type of variable cannot be reassigned?",
-        answers: {
-            a: "var",
-            b: "let",
-            c: "const",
-            d: "none"
-        },
-        correctAnswer: 'b'
-    },
-
-    {
-        question: "Timers in Javascript are typically measured in which unit of time?",
-        answers: {
-            a: "minutes",
-            b: "seconds",
-            c: "flops",
-            d: "milliseconds"
-        },
-        correctAnswer: 'd'
-    },
-    {
-        question: "What common structure does the DOM resemble?",
-        answers: {
-            a: "house",
-            b: "shed",
-            c: "shrub",
-            d: "tree"
-        },
-        correctAnswer: 'd'
-    },
-
-    {
-        question: "What company developed Javascript?",
-        answers: {
-            a: "Apple",
-            b: "Microsoft",
-            c: "IBM",
-            d: "Netscape"
-        },
-        correctAnswer: 'd'
-    },
-
-    {
-        question: "Which of these are not a Javascript Event Handler?",
-        answers: {
-            a: "onclick",
-            b: "onmouseover",
-            c: "onsubmit",
-            d: "continue"
-        },
-         correctAnswer: 'd'
-    },
+//variables
+//questions index
+//questions element for questions container
+//timer
+//choices definition
+//submit button definition
+//start button definition
+//enter player name
+//track high score on leaderboard
+//questions array here
+  //title
+  //choices in an array
+  //correct answer
 
 
-  ];
-  
-//Variables
-var score = 0;
-var questionIndex = 0;
+//functions
 
-var secondsRemaining = 75;
-var penalty = 10;
+//time interval
 
-//puts a question and answer to the page
 
-function render(questionIndex) {
-questions.Div.innerHTML = "";
-ulCreate.innerHTML = "";
+//getQuestion
 
-for (var i=0; i < questions.length; i++) {
-  var userQuestion = questions[questionIndex].stem;
-  var userChoices = questions[questionIndex].choices;
 
-  questionsDiv.textContent = userQuestion;
+//checkAnswer
+
+
+//update leaderboard
+function updateLeaderboard () {
+var name = nameEL.value.trim();
+console.log(name);
+var bestScores = JSON.parse(window.localStorage.getItem("leaderBoard")) || []
+
+var playerScore = {
+score: time,
+name: name,
 }
 
-userChoices.forEach(function (newItem) {
-var listItem = document.createElement("li");
-listItem.textContent = newItem;
-questionsDiv.appendChild(ulCreate);
-ulCreate.appendChild(listItem);
-listItem.addEventListener("click", (compare));
-})
+bestScores.push(playerScore);
+window.localStorage.setItem("leaderBoard", JSON.stringify(leaderBoard));
+window.location.href - "./leaderboard.html";
+};
 
-}
+//player submits name
+submitBtn.onclick = updateLeaderboard;
+startBtn.online = start;
 
-//check correct answer
-
-function compare(event) {
-  var element = event.target;
-
-  if (element.matches ("li")) {
-
-    var createDiv = document.createElement("div")
-    createDiv.setAttribute("id", "createDiv");
-
-    if (element.textContent === questions[questionIndex].correct) {
-      score++;
-      createDiv.textContent = "Yes, the answer is" + questions[questionIndex].correct;
-    }
-    else {
-      addPenalty = secondsRemaining - penalty; 
-      createDiv.textContent = "No, the answer is" + questions[questionIndex].correct;
-
-questionIndex++;
-
-if (questionIndex >= questions.length) {
-  quizComplete (); 
-
-  createDiv.textContent = "Quiz Completed"
-//add score here
-
-  }
-}
-
-
-
-
-  //aligning to html
-  var quizContainer = document.getElementById('quiz');
-  var resultsContainer = document.getElementById('results');
-  var submitButton = document.getElementById('submit');
-  var counter = 0;
-  
-  generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
-  
-  function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-  
-    function showQuestions(questions, quizContainer){
-      // we'll need a place to store the output and the answer choices
-      var output = [];
-      var answers;
-  
-      // for each question...
-   
-        
-        // first reset the list of answers
-        answers = [];
-  
-        // for each available answer...
-        for(letter in questions[counter].answers){
-  
-          // ...add an html radio button
-          answers.push(
-            '<label>'
-              + '<input type="radio" name="question'+0+'" value="'+letter+'">'
-              + letter + ': '
-              + questions[counter].answers[letter]
-            + '</label>'
-          );
-        }
-  
-        // add this question and its answers to the output
-        output.push(
-          '<div class="question">' + questions[counter].question + '</div>'
-          + '<div class="answers">' + answers.join('') + '</div>'
-        );
-      
-  
-      // finally combine our output list into one string of html and put it on the page
-      quizContainer.innerHTML = output.join('');
-    }
-  
-  
-    function showResults(questions, quizContainer, resultsContainer){
-      
-      // gather answer containers from our quiz
-      var answerContainers = quizContainer.querySelectorAll('.answers');
-      
-      // keep track of user's answers
-      var userAnswer = '';
-      var numCorrect = 0;
-      
-      // for each question...
-      
-  
-        // find selected answer
-        //userAnswer = (answerContainers[counter].querySelector([name=question'+counter+']:checked')||{}).value;)
-        
-        // if answer is correct
-        if(userAnswer===questions[counter].correctAnswer){
-          // add to the number of correct answers
-          numCorrect++;
-          
-          // color the answers green
-          answerContainers[i].style.color = 'lightgreen';
-        }
-        // if answer is wrong or blank
-        else{
-          // color the answers red
-          answerContainers[counter].style.color = 'red';
-        }
-      
-  
-      // show number of correct answers out of total
-      resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
-    }
-  
-    // show questions right away
-    showQuestions(questions, quizContainer);
-
-    
-    // on submit, show results
-    submitButton.onclick = function(){
-      counter++;
-      showResults(questions, quizContainer, resultsContainer);
-      generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
-    }
-  
-  }
-
-  //counter
- var startTime= 75;
-
- var x = setInterval(function () {
-   startTime--;
-  
-   document.getElementById("time").innerText = startTime;
- 
-   if (startTime < 0) {
-     clearInterval(x);
-     document.getElementById("time").innerHTML = "EXPIRED"
-   }
- }, 1000 );
-  }}
